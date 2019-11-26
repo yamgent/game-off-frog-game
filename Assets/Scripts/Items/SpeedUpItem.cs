874b4e9c87ca38af7aad45dca92afffaaaa84562
initialshl <initialshl@outlook.com>
2019-11-25 21:53:32 -0800

    Change speedUp to factor 1.05f

--- Assets/Scripts/Items/SpeedUpItem.cs
+++ Assets/Scripts/Items/SpeedUpItem.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class SpeedUpItem : MonoBehaviour
 {
-    public float speedUpAmount;
+    public float speedUpFactor;
+
+    private Level level;
 
     private bool isSpawned = false;
     private int row = -1;
     private int col = -1;
 
+    void Start() {
+        level = Level.GetSingleton();
+    }
+
     void Update() {
-        if (isSpawned && !Level.GetSingleton().HasLilypadAt(row, col)) {
+        if (isSpawned && !level.HasLilypadAt(row, col)) {
             Destroy(gameObject);
         }
     }
 
     void OnTriggerEnter2D(Collider2D col) {
         if (col.CompareTag("Player")) {
-            col.GetComponent<Frog>().speed += speedUpAmount;
+            col.GetComponent<Frog>().speed *= speedUpFactor;
             col.GetComponent<Frog>().CalculateNewJumpTime();
             Destroy(gameObject);
         }
