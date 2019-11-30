c7740f2cbb68ecb299c4eade365914b759845694
initialshl <initialshl@outlook.com>
2019-11-30 11:43:23 -0800

    Add dragonfly move script template

---
+++ Assets/Scripts/Items/AutoMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Move upwards whenever it is near or within the camera bounds.
+
+        // Speed is relative to current camera speed.
+
+        // Die when it is lagging too far from camera.
+    }
+}
