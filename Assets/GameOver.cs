df36082df35a284dca9b902e6a285e1062dd971f
Ian Teo <ian_teo@msn.com>
2019-12-01 14:15:38 +0800

    Add game over screen when frog enters dead animation

---
+++ Assets/GameOver.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class GameOver : StateMachineBehaviour {
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        PauseButton.GetSingleton().Pause(true);
+    }
+}
